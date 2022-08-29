0e980b6cfc2d63860d318f0afd18af7089b34a0b
Hui Miao <huimiao@microsoft.com>
2022-08-29 22:46:01 +0800

    feat: adjust banner widget style

--- tabs/src/widget/Banner.tsx
+++ tabs/src/widget/Banner.tsx
@@ -1,28 +1,50 @@
-import './Styles.css';
+import "./Styles.css";
 
-import { Avatar, Body1, Caption1, Image } from '@fluentui/react-components';
-import { Card, CardHeader, CardPreview } from '@fluentui/react-components/unstable';
-import { Comment16Regular } from '@fluentui/react-icons';
+import { Avatar, Image, Label } from "@fluentui/react-components";
+import { Card, CardPreview } from "@fluentui/react-components/unstable";
+import {
+  Earth20Regular,
+  Person20Regular,
+  Star20Regular,
+} from "@fluentui/react-icons";
 
 export default function Banner() {
   return (
-    <div style={{ width: "100%" }}>
+    <div className="banner">
       <Card>
-        <CardPreview
-          logo={<Image alt="app logo" src="logoipsum-223.svg" fit="cover" />}
-        >
-          <Image alt="file preview" src="900x100.jpg" />
+        <CardPreview className="banner-img">
+          <Image alt="file preview" src="top-img.jpg" fit="cover"/>
         </CardPreview>
-        <CardHeader
-          image={<Avatar image={{ src: "20x20.jpg" }} />}
-          header={<Body1 weight="semibold">Classroom Collaboration</Body1>}
-          description={
-            <Caption1 className="bannerContainer">
-              <Comment16Regular color="#D83B01" />
-              <span>Colin replied to a comment</span>
-            </Caption1>
-          }
-        />
+        <div className="banner-org-container">
+          <Avatar image={{ src: "contoso.png" }} size={56} />
+          <div className="banner-org-container-des">
+            <Label weight="semibold" size="large">
+              Contoso Team
+            </Label>
+            <div className="banner-org-container-desc-tooltip">
+              <div className="banner-org-container-desc-tooltip-item">
+                <Earth20Regular />
+                <Label size="small">project.contoso.com</Label>
+              </div>
+              <div className="banner-org-container-desc-tooltip-item">
+                <Person20Regular />
+                <Label size="small">Team Lead: Contoso contoso</Label>
+              </div>
+              <div className="banner-org-container-desc-tooltip-item">
+                <Star20Regular />
+                <Label size="small">Service Type: Process Transformation</Label>
+              </div>
+            </div>
+            <Label>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque mattis rutrum enim, eget vestibulum nisl fringilla
+              in. Nullam nec justo fringilla, lobortis lectus in, interdum diam.
+              Donec tempor, neque vel tincidunt volutpat, tellus odio ultricies
+              dui, eu ultricies augue diam a odio. Suspendisse erat arcu, dictum
+              eleifend aliquet nec, iaculis maximus ante.{" "}
+            </Label>
+          </div>
+        </div>
       </Card>
     </div>
   );
