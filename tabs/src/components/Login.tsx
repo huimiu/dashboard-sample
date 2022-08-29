6ca62db90c7b67161a8bebd2faae56aace4ac389
frankqianms <frankqian@microsoft.com>
2022-08-29 17:28:00 +0800

    .

--- tabs/src/components/Login.tsx
+++ tabs/src/components/Login.tsx
@@ -1,7 +1,44 @@
 import './Styles.css';
 
 import { Escape, View } from '@fluent-blocks/react';
-import { Button } from '@fluentui/react-components';
+import { createMicrosoftGraphClient, TeamsFx, UserInfo } from '@microsoft/teamsfx';
+import { dashboardTeamsFxContext } from "./Context";
+import { Client } from "@microsoft/microsoft-graph-client";
+import UserprofileModel from '../model/UserprofileModel';
+
+const scope = ["User.Read"];
+
+async function loginAction() {
+  const teamsfx = new TeamsFx();
+  try {
+    await teamsfx.login(scope);
+    // store the teamsfx globally
+    dashboardTeamsFxContext.setTeamsfx(teamsfx);
+  } catch(e) {
+    console.log(e);
+    throw "Login Error: can not login!";
+  }
+}
+
+var profile: any;
+async function getUserprofile() {
+  const teamsfx = new TeamsFx();
+  try {
+    const token = await dashboardTeamsFxContext.getTeamsfx()?.getCredential().getToken(scope);
+    let tokenstr;
+    if (!token) tokenstr = ""; 
+    else tokenstr = token.token;
+    teamsfx.setSsoToken(tokenstr);
+  } catch(e) {}
+
+  try {
+    const graphClient: Client = createMicrosoftGraphClient(teamsfx, [".default"]);
+    profile = await graphClient.api("/me").get();
+    console.log(profile);
+  } catch(e) {}
+
+  //return profile;
+}
 
 export default function Login() {
   return (
@@ -19,7 +56,10 @@ export default function Login() {
                   label: "Login",
                   variant: "primary",
                   onAction: () => {
-                    alert("clicked");
+                    loginAction();
+                    getUserprofile();
+                    // const profilestr = profile.toString();
+                    // const getProfile = (): UserprofileModel => JSON.parse(profilestr);
                   },
                 },
               },
