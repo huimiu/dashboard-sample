6aa376d8c988f4249aae0c9a66ed935425021eb5
frankqianms <frankqian@microsoft.com>
2022-09-26 10:11:11 +0800

    chore: delete unneccessary codes & README

--- tabs/src/service/GetFiles.ts
+++ tabs/src/service/GetFiles.ts
@@ -60,23 +60,14 @@ export async function getFiles() {
   let teamsfx: TeamsFx;
   try {
     teamsfx = FxContext.getInstance().getTeamsFx();
-    const token = await teamsfx?.getCredential().getToken(scope);
-    let tokenstr = "";
-    if (token) tokenstr = token.token;
-    teamsfx.setSsoToken(tokenstr);
-  } catch (e) {
-    throw e;
-  }
-
-  try {
     const graphClient: Client = createMicrosoftGraphClient(teamsfx, scope);
     const drives = await graphClient
       .api(
         "/me/drive/recent?$top=5&$select=name,webUrl,createdBy,lastModifiedBy,remoteItem"
       )
       .get();
-    const driveInfo = drives["value"];
 
+    const driveInfo = drives["value"];
     let returnAnswer: FilesModel[] = [];
     for (const obj of driveInfo) {
       const tmp: FilesModel = {
